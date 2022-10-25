defdc5b5fdb11b0d38c66380fe0b036e27e68194
Diego Hinagas <hinagasrodriguez@gmail.com>
2022-10-24 21:12:45 -0400

    added new features for display and managment form for new blog

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -4,18 +4,24 @@ import Notification from './Notification'
 import BlogForm from './BlogForm'
 
 import blogService from '../services/blogs'
+import Togglable from './Togglable'
 
 const Blogs = ({ blogs, user, handleUser, handleBlogs }) => {
   const [message, setMessage] = useState(null)
   const [type, setType] = useState('')
+  const [visible, setVisible] = useState(false);
 
   const handleLogout = () => {
     window.localStorage.removeItem('loggedBloglistUser')
     handleUser(null)
   }
 
+  const handleVisible = () => setVisible(!visible)
+
   const addNewBlog = async (blog) => {
     try {
+      setVisible(false);
+      
       const newBlog = await blogService.create(blog)
 
       handleBlogs(blogs => [...blogs, newBlog])
@@ -48,7 +54,9 @@ const Blogs = ({ blogs, user, handleUser, handleBlogs }) => {
         <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
       </div>
       
-      <BlogForm addNewBlog={addNewBlog} />
+      <Togglable buttonLabel="new note" visible={visible} handleVisible={handleVisible}>
+        <BlogForm addNewBlog={addNewBlog} />
+      </Togglable>
       
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
