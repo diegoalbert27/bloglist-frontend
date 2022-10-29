6d0339d9c3c741cfc655eaef8d2231cb308e7cd5
Diego Hinagas <hinagasrodriguez@gmail.com>
2022-10-29 13:48:17 -0400

    added new test for blog component

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,6 +1,5 @@
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
-import { prettyDOM } from '@testing-library/dom'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 const blog = {
@@ -11,17 +10,44 @@ const blog = {
 }
 
 test('show only title and author for blog per default without likes or url', () => {
-  const { container } = render(
+  const component = render(
     <Blog blog={blog} addNewLike={() => {}} removeBlog={() => {}} />
   )
 
-  const blogTitle = container.querySelector('.blog-title')
+  const blogTitle = component.container.querySelector('.blog-title')
 
   expect(blogTitle).toBeDefined()
   expect(blogTitle).toBeVisible()
   expect(blogTitle).toHaveTextContent(`${blog.title} by ${blog.author}`)
 
-  const blogDetails = container.querySelector('.blog-details')
+  const blogDetails = component.container.querySelector('.blog-details')
   expect(blogDetails).toBeDefined()
   expect(blogDetails).not.toBeVisible()
+})
+
+test('see likes and url when click on view', () => {
+  const component = render(<Blog blog={blog} addNewLike={() => {}} removeBlog={() => {}} />)
+
+  const button = component.getByText('view')
+  fireEvent.click(button)
+
+  const blogDetails = component.container.querySelector('.blog-details')
+  expect(blogDetails).toBeVisible()
+  expect(blogDetails).toHaveTextContent(`${blog.url}`)
+  expect(blogDetails).toHaveTextContent(`${blog.likes}`)
+})
+
+test('click the like button twice and fire your controller twice', () => {
+  const mockHandler = jest.fn()
+  
+  const component = render(<Blog blog={blog} addNewLike={mockHandler} removeBlog={() => {}} />)
+
+  const buttonView = component.getByText('view')
+  fireEvent.click(buttonView)
+
+  const buttonLike = component.getByText('like')
+  fireEvent.click(buttonLike)
+  fireEvent.click(buttonLike)
+  
+  expect(mockHandler.mock.calls).toHaveLength(2)
 })
